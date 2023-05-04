b1c743ece61599dc031cecdd33364c527285dc69
hagai.arad <hagai.arad@aiola.com>
2023-05-04 15:41:53 +0300

    add partition cols to func

--- common_utils/sql_methods.py
+++ common_utils/sql_methods.py
@@ -1,6 +1,9 @@
 class sqlMethods:
     def create_athena_table_statement(
-        table_name: str, col_types: dict[str, str], s3_path: str
+        table_name: str,
+        col_types: dict[str, str],
+        s3_path: str,
+        partition_cols: dict[str, str] = {},
     ) -> str:
         """
         Generates a SQL CREATE TABLE IF NOT EXISTS statement for Athena based on a dictionary of table columns and their data
@@ -14,10 +17,19 @@ class sqlMethods:
         Returns:
             str: A SQL CREATE TABLE statement string.
         """
+
         columns_sql = ", ".join([f"{col} {dtype}" for col, dtype in col_types.items()])
+
+        partition_cols_sql = ", ".join(
+            [f"{col} {dtype}" for col, dtype in partition_cols.items()]
+        )
+        partition_clause = (
+            f"PARTITIONED BY ({partition_cols_sql})" if partition_cols_sql else ""
+        )
 
         sql_statement = f"""
                             CREATE EXTERNAL TABLE IF NOT EXISTS {table_name} ({columns_sql}) 
+                            {partition_clause}
                             LOCATION '{s3_path}'
                         """
         return sql_statement
